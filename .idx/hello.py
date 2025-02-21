e8815db33b329f5b24b12cab78fda4b68bef3a9f
dipot.png <dipot.png@gmail.com>
2025-02-21 02:20:54 +0000

    Initial commit

---
+++ .idx/hello.py
@@ -0,0 +1,21 @@
+nama = 'Diva Ardian'
+umur = 20
+status = False
+
+print("Halo semuanya")
+print("Namaku",nama,"Umurku",umur,"tahun")
+if (status):
+    print('Status : Menikah')
+else:
+    print('Status : Belum Menikah')
+print("Mulai hari ini Aku bertekat untuk mempelajari AI")
+print("Untuk minggu ini dimulai dari Pemrograman Pyhton dahulu")
+print("Semoga tekat ku tidak pudar dan akan konsisten sampai diriku mahir")
+
+v1 = "ini variable 1, variable merupakan tempat menyimpan data."
+v2 = """
+    Jl. Karet Komp. Patra Permai B.11
+"""
+print(v1)
+print(v2)
+print("Itu tadi adalah contoh dari variable alamat.")
